41902a16e56cd7b9b53777f8840bcda2faaa86c4
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2025-01-28 23:05:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://loki-28jan-0b8bedaeaa60.herokuapp.com/")
+URL = environ.get("FQDN", "")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
